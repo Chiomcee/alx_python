a34e08c51ade987ee27d9771f36e66fca91d97a5
Chioma Igwe <cigwe072012@gmail.com>
2023-07-23 20:57:45 -0700

    nameerror exception

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def raise_exception_msg(message=""):
-    # Raise a nameError with a custom message
+    """Raise a nameError with a custom message"""
     raise NameError(message)
