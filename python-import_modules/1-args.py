c30f1e9e06ea78266810552fcd0b69f8b7e294da
Chioma Igwe <cigwe072012@gmail.com>
2023-07-23 20:04:51 -0700

    pycodestyle update

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
 import sys
 
+
 if __name__ == "__main__":
-    args = sys.argv[1:] # Exclude the script name from the arguments
+    args = sys.argv[1:]   # Exclude the script name from the arguments
     # Determine the number of arguments
     num_args = len(args)
 
-    #print the number of arguments
+    # print the number of arguments
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
@@ -16,4 +18,3 @@ if __name__ == "__main__":
     # print the arguments
     for i, arg in enumerate(args):
         print("{}: {}".format(i + 1, arg))
-    
