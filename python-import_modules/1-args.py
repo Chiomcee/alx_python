5318cb7ee994ab191918f8a6fe9d7a9728dfa6c4
Chioma Igwe <cigwe072012@gmail.com>
2023-07-23 19:54:49 -0700

    argument printing

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:] # Exclude the script name from the arguments
+    # Determine the number of arguments
+    num_args = len(args)
+
+    #print the number of arguments
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+    # print the arguments
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i + 1, arg))
+    
