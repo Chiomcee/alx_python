e791b85cfa7be583dc511a47b011fffdae59034d
Chioma Igwe <cigwe072012@gmail.com>
2023-07-24 20:02:16 -0700

    updated

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, num in enumerate(row):
-            if i != len(row) - 1:
-                print("{:d}".format(num), end="")
+        for value in row:
+            if value == row[-1]:
+                print("{:d}".format(value), end='')
             else:
-                print("{:d}".format(num))
+                print("{:d}".format(value), end=' ')
         print()
