5b051c9aef583f2e75577799e03e0ff37d2ad5aa
Chioma Igwe <cigwe072012@gmail.com>
2023-07-31 06:26:15 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Representing the class Square"""
+
+
+class Square:
+    """Defines a class square."""
+
+    def __init__(self, size=0):
+        """Initializing a new object square.
+
+        Args:
+            size(int):must be an integer,
+            represents size of the square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
