774c010e0528565327bb31f0976b773518981621
Chioma Igwe <cigwe072012@gmail.com>
2023-07-31 02:28:41 -0700

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Representing the class Square"""
 
+
 class Square:
     """
-    This takes  a size parameter and 
-    store in a private instance attribute named '__size' 
+    This takes  a size parameter and
+    store in a private instance attribute named '__size'
     using the double underscore prefix
     """
 
