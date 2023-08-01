cc02e8cd1b11098f96b6d6045a0bd2c860d7005c
Chioma Igwe <cigwe072012@gmail.com>
2023-08-01 03:22:05 -0700

    class with area method

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a base geometry class"""
+
+
+class BaseGeometry:
+    """Represents a base geometry class """
+
+
+    def area(self):
+        """Area not yet implemented"""
+        raise Exception("area() is not implemented")
