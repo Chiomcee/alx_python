64f3e4b1ecf9412ed9f371f88a49dfae0bbb9404
Chioma Igwe <cigwe072012@gmail.com>
2023-08-02 01:47:39 -0700

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,6 +4,19 @@
 
 class BaseGeometry:
     """Creates an empty class"""
+    def __class_getitem__(cls, params):
+        return cls
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __dir__(self):
+        class_dir = dir(type(self))
+        class_dir.remove("__init_subclass__")
+        return class_dir
+
+    def __sizeof__(self):
+        return object.__sizeof__(self)
 
     
     def __dir__(cls):
