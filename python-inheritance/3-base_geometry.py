272d492d1cc117b618ed23a85e4dadd29e5a43bd
Chioma Igwe <cigwe072012@gmail.com>
2023-08-02 01:28:48 -0700

    attri init_subclass removed

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,4 +9,4 @@ class BaseGeometry:
     def __dir__(cls):
         # get list of all attributes for this classand exclude_init_subclass
         attributes = super().__dir__()
-        return [attributes for attributes in attributes if attributes != '__init__subclass__']
+        return [attributes for attributes in attributes if attributes != '__init_subclass__']
