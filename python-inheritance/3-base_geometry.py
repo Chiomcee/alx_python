607b256072eaadaeded34b9c389837e20824c094
Chioma Igwe <cigwe072012@gmail.com>
2023-08-02 01:52:41 -0700

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,17 +4,8 @@
 
 class BaseGeometry:
     """Creates an empty class"""
-    def __class_getitem__(cls, params):
-        return cls
 
-    def __str__(self):
-        return str(self.__dict__)
-
-    def __dir__(self):
-        class_dir = dir(type(self))
-        class_dir.remove("__init_subclass__")
-        return class_dir
-
-    def __sizeof__(self):
-        return object.__sizeof__(self)
-
+    def __dir__(cls):
+        # get list of all attributes for this classand exclude_init_subclass
+        attributes = super().__dir__()
+        return [attributes for attributes in attributes if attributes != '__init_subclass__']
