78801fac922b5e3a3db95fb899de37eb61eadc20
Chioma Igwe <cigwe072012@gmail.com>
2023-08-02 02:09:51 -0700

    update

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -7,11 +7,6 @@ BaseGeometry = __import__('5-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Represents a rectangle using BaseGeometry attributes and methods"""
 
-    def __dir__(cls):
-        # get list of all attributes for this classand exclude_init_subclass
-        attributes = super().__dir__()
-        return [attributes for attributes in attributes if attributes != '__init_subclass__']
-
     def __init__(self, width, height):
         """Initializes a new Rectangle"""
 
