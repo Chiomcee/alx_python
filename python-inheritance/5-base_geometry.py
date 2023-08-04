d357ff6653029b043646f37777174014ce6be756
Chioma Igwe <cigwe072012@gmail.com>
2023-08-04 12:35:40 -0700

    update

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -2,8 +2,16 @@
 """Defines a base geometry class"""
 
 
-class BaseGeometry:
-    """Represents a base geometry class """
+class BaseGeometryMetaClass(type):
+    """Class to override the init_subclasss in base geometry class"""
+    def __dir__(cls):
+        # get list of all attributes for this classand exclude_init_subclass
+        attributes = super().__dir__()         
+        return [attributes for attributes in attributes if attributes != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """Creates an empty class"""
 
     def __dir__(cls):
         # get list of all attributes for this classand exclude_init_subclass
