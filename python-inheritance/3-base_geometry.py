f0bd48fe2e196dc7c784a3957a67b6ea682e1591
Chioma Igwe <cigwe072012@gmail.com>
2023-08-04 12:09:14 -0700

    using a meta class to resolve init_subclass removal from the parent class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """Defines an empty class"""
 
+class BaseGeometryMetaClass(type):
+    """Class to override the init_subclasss in base geometry class"""
+    def __dir__(cls):
+        # get list of all attributes for this classand exclude_init_subclass
+        attributes = super().__dir__()         
+        return [attributes for attributes in attributes if attributes != '__init_subclass__']
 
-class BaseGeometry:
+
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
     """Creates an empty class"""
 
     def __dir__(cls):
