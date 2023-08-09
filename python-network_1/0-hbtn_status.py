07b0c1eacb0685e8d2f88cedc1ae6a1fb6f1d630
Chioma Igwe <cigwe072012@gmail.com>
2023-08-09 13:04:18 -0700

    fetching requests

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,6 +10,5 @@ url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 
 print("Body response:")
-print(f"   - type: {type(response.text)}")
-print(f"   - content: {response.text}")
-
+print(f"    - type: {type(response.text)}")
+print(f"    - content: {response.text}")
