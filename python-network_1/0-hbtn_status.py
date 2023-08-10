d91feabe0764335e01bd98a5414a5cbf0faaad69
Chioma Igwe <cigwe072012@gmail.com>
2023-08-10 04:37:29 -0700

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,13 +3,12 @@
     Scripts that uses the requests package to 
     fetch the status from the specified URL
 """
-import urllib.request
+import requests
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+url = "https://alu-intranet.hbtn.io/status"
+
+response = requests.get(url)
+
+print("Body response:")
+print(f"    - type: {type(response.text)}")
+print(f"    - content: {response.text}")
