06ad2277b717ef67e1f9f20539b1a04dc831c5c7
Chiomcee <cigwe072013@gmail.com>
2023-08-10 23:02:33 +0100

    Display Id with Github API

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    script that takes your GitHub credentials (username and password) 
+    and uses the GitHub APIto display your id
+"""
+import requests
+import sys
+
+"""Get the username and password from command line arguments"""
+username = sys.argv[1]
+password = sys.argv[2]
+
+"""Set the URL to access user information"""
+url = "https://api.github.com/user"
+
+"""Set the headers with Basic Authentication using the username and password"""
+headers = {'Authorization': 'Basic {}:{}'.format(username, password)}
+
+"""Send a GET request to the URL with Basic Authentication headers"""
+response = requests.get(url, headers=headers)
+
+"""Check if the request was successful"""
+if response.status_code == 200:
+    """Retrieve the user id from the response"""
+    user_id = response.json()['id']
+
+    """Display the user id"""
+    print(user_id)
+else:
+    """Display None if the request failed"""
+    print("None")
