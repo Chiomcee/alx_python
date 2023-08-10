1a23a94fc476ef326f7d9f9155a087a0e0d66c3b
Chiomcee <cigwe072013@gmail.com>
2023-08-10 21:39:33 +0100

    update

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -4,7 +4,7 @@ number = random.randint(-10, 10)
 # Check if the number is positive, negative, or zero
 if number > 0:
     print(number, "is positive")
-elif number == 0:
-    print(number, "is zero")
-else:
+elif number < 0:
     print(number, "is negative")
+else:
+    print(number, "is zero")
--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,8 +10,8 @@ url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 if response.status_code == 200:
     print("Body response:")
-    print("\t - type: {}".format(type(response.text)))
-    print("\t - content: {}".format(response.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
 
 else:
     print("Error: {}".format(response.status_code))
