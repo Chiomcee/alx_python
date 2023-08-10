a81438a2b3d7946843012345f2c54a9363636ff3
Chiomcee <cigwe072013@gmail.com>
2023-08-10 21:34:17 +0100

    get request

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,7 +8,10 @@ import requests
 url = "https://alu-intranet.hbtn.io/status"
 
 response = requests.get(url)
+if response.status_code == 200:
+    print("Body response:")
+    print("\t - type: {}".format(type(response.text)))
+    print("\t - content: {}".format(response.text))
 
-print("Body response:")
-print(f"    - type: {type(response.text)}")
-print(f"    - content: {response.text}")
+else:
+    print("Error: {}".format(response.status_code))
