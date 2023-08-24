dd81ca298a1db0829723cb97988e298f942d47b6
Chioma Igwe <cigwe072013@gmail.com>
2023-08-24 13:56:47 -0700

    update

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-    This script takes in an argument and displays all values i
-    n the states table of hbtn_0e_0_usa where name matches the argument.
+This script takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
@@ -9,19 +8,16 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
     )
-    cursor = db.cursor()
-   query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                        states.id ASC".format(argv[4])
-    cursor.execute(query)
-
-    for row in cursor.fetchall():
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4])
+    cur.execute(query)
+    for row in cur.fetchall():
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
