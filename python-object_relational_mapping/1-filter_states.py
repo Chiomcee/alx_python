20b064d2ef9d63bb9293f25fa4bc2c48db059914
Chioma Igwe <cigwe072013@gmail.com>
2023-08-23 19:35:42 -0700

    list all states with name starting with upper N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connection to database server
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    # Cursor object creation
+    cursor = db.cursor()
+
+    # Query execution
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Results printing
+    for row in cursor.fetchall():
+        print(row)
+
+    # Clean up
+    cur.close()
+    db.close()
