71b2b7f6faff6fbc1c90d104ba7dd02a5682cb3b
Chioma Igwe <cigwe072013@gmail.com>
2023-08-23 19:01:35 -0700

    lists all states from database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,12 @@ import sys
 if __name__ == '__main__':
     # Connect to the database
     db = MySQLdb.connect(
-            host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+    )
 
     # Create a cursor object
     cursor = db.cursor()
