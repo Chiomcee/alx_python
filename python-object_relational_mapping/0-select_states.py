b19334bc31cf55a5cf6855ca010bf69a20ce0840
Chioma Igwe <cigwe072013@gmail.com>
2023-08-23 18:52:29 -0700

    lists all states from database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    List all states from database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute a SELECT query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch the results
+    results = cursor.fetchall()
+
+    # Print the results
+
+    for row in results:
+        print(row)
+
+    # Close the database connection
+    db.close()
+
