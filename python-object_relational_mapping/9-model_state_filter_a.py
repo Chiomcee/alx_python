a16f99ca445c384c852f483d7147e9a71185d280
Chiomcee <cigwe072013@gmail.com>
2023-08-26 11:14:25 +0100

    updates on pep

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,16 +13,15 @@ if __name__ == '__main__':
     # Create engine. Args: [1]username, [2]password, [3]database_name
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
-                           
+                           pool_pre_ping=True) 
 
     # Create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query State objects containing 'a' and print them
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
