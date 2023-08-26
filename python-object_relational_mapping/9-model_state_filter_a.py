82cb7aaadb439c83bab8cb8e535e4f80cdae8db6
Chiomcee <cigwe072013@gmail.com>
2023-08-26 11:18:57 +0100

    update

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     # Create engine. Args: [1]username, [2]password, [3]database_name
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+                           pool_pre_ping=True)
 
     # Create session
     Session = sessionmaker(bind=engine)
