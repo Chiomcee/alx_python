60154aa3a5799dfebaa08c49f6a6f2061cb6d81f
Chiomcee <cigwe072013@gmail.com>
2023-08-26 03:11:29 +0100

    link class to table in db

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The script defines a class called State that inherits 
+from the declarative_base() class in SQLAlchemy.
+It also imports the necessary modules to define the class.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'state'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
