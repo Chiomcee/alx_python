365fbad50fe984d242d889f4b3902fb193cfda40
Chiomcee <cigwe072013@gmail.com>
2023-08-29 03:20:41 +0100

    adding another function

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Script that starts a Flask Web 
+   application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route specifies the root URL '/':display "Hello HBNB!"
+
+    Returns:
+    Str:  the string containg the greeting "Hello HBNB!"
+    """
+    return"Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route specifies the root URL '/':display "hbnb"
+
+    Returns:
+    Str:  the string containg the message "HBNB"
+    """
+    return "HBNB"
+
+if __name__ == '__main__':
+    """
+    checks if the current script is being run directly
+    starts the Flask web application
+    """
+    app.run(host='0.0.0.0',port=5000)
