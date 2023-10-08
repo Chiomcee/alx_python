77e4473c0a72ce06788c56571a39dd53a57cf4a8
Chioma Igwe <cigwe072013@gmail.com>
2023-10-08 01:19:12 +0100

    update

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -9,30 +9,34 @@ import sys
 if __name__ == "__main__":
     employee_ID = int(sys.argv[1])
     base_url = 'https://jsonplaceholder.typicode.com'
-    users_url = '{}/users'.format(base_url)
-    users_response = requests.get(users_url)
-    users_data = users_response.json()
+    users_url = f'{base_url}/users/{employee_ID}'
+    todo_url = f'{base_url}/todos?userId={employee_ID}'
 
-    matching_user = None
-    for employee in users_data:
-        if employee['id'] == employee_ID:
-            matching_user = employee
-            break
-    if matching_user:
-        employee_name = matching_user['name']
-        todo_url = f'{base_url}/todos?userId={employee_ID}'
+    try:
+        user_response = requests.get(users_url)
         todo_response = requests.get(todo_url)
+        user_response.raise_for_status()
+        todo_response.raise_for_status()
+        user_data = user_response.json()
         todo_data = todo_response.json()
-        completed = []
-        for todo in todo_data:
-            if todo['completed']:
-                completed.append(todo)
+
+        employee_name = user_data['name']
         json_data = {str(employee_ID): []}
+        
         for task in todo_data:
             json_data[str(employee_ID)].append({
-                "task": task['title'],
-                "completed": task['completed'],
-                "username": employee_name})
-        json_filename = "{}.json".format(employee_ID)
-        with open(json_filename, "w") as file:
+                'task': task['title'],
+                'completed': task['completed'],
+                'username': employee_name
+            })
+        
+        json_filename = f'{employee_ID}.json'
+        
+        with open(json_filename, 'w') as file:
             json.dump(json_data, file)
+
+        print(f'Tasks for user {employee_ID} exported to {json_filename}')
+    
+    except requests.exceptions.RequestException as e:
+        print(f'Error occurred: {e}')
+
